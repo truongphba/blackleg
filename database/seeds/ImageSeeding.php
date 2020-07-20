17a9f17c76ade22785a660d0daa3a00e06f8da39
truongph-fpt <truongphth1907039@fpt.edu.vn>
2020-07-20 21:01:31 +0700

    update ImageSeeding

---
+++ database/seeds/ImageSeeding.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ImageSeeding extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
