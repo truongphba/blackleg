6a219b1fd96dfffa0750c19d99790c1423394a7b
quannguyen1778 <59248721+quannguyen1778@users.noreply.github.com>
2020-07-28 16:55:44 +0700

    thêm seeder cho sản phẩm

--- database/seeds/ProductSizeSeeder.php
+++ database/seeds/ProductSizeSeeder.php
@@ -257,6 +257,401 @@ class ProductSizeSeeder extends Seeder
                 'size_id' => 4,
                 'quantity' => 2,
             ],
+            [
+                'product_id' => 19,
+                'size_id' => 1,
+                'quantity' => 1,
+            ],
+            [
+                'product_id' => 19,
+                'size_id' => 2,
+                'quantity' => 5,
+            ],
+            [
+                'product_id' => 19,
+                'size_id' => 3,
+                'quantity' => 3,
+            ],
+            [
+                'product_id' => 19,
+                'size_id' => 4,
+                'quantity' => 2,
+            ],
+            [
+                'product_id' => 20,
+                'size_id' => 1,
+                'quantity' => 4,
+            ],
+            [
+                'product_id' => 20,
+                'size_id' => 2,
+                'quantity' => 2,
+            ],
+            [
+                'product_id' => 20,
+                'size_id' => 3,
+                'quantity' => 3,
+            ],
+            [
+                'product_id' => 20,
+                'size_id' => 4,
+                'quantity' => 4,
+            ],
+            [
+                'product_id' => 21,
+                'size_id' => 1,
+                'quantity' => 2,
+            ],
+            [
+                'product_id' => 21,
+                'size_id' => 2,
+                'quantity' => 6,
+            ],
+            [
+                'product_id' => 21,
+                'size_id' => 3,
+                'quantity' => 1,
+            ],
+            [
+                'product_id' => 21,
+                'size_id' => 4,
+                'quantity' => 5,
+            ],
+            [
+                'product_id' => 22,
+                'size_id' => 1,
+                'quantity' => 3,
+            ],
+            [
+                'product_id' => 22,
+                'size_id' => 2,
+                'quantity' => 2,
+            ],
+            [
+                'product_id' => 22,
+                'size_id' => 3,
+                'quantity' => 4,
+            ],
+            [
+                'product_id' => 22,
+                'size_id' => 4,
+                'quantity' => 2,
+            ],
+            [
+                'product_id' => 23,
+                'size_id' => 1,
+                'quantity' => 3,
+            ],
+            [
+                'product_id' => 23,
+                'size_id' => 2,
+                'quantity' => 4,
+            ],
+            [
+                'product_id' => 23,
+                'size_id' => 3,
+                'quantity' => 2,
+            ],
+            [
+                'product_id' => 23,
+                'size_id' => 4,
+                'quantity' => 6,
+            ],
+            [
+                'product_id' => 24,
+                'size_id' => 1,
+                'quantity' => 1,
+            ],
+            [
+                'product_id' => 24,
+                'size_id' => 2,
+                'quantity' => 5,
+            ],
+            [
+                'product_id' => 24,
+                'size_id' => 3,
+                'quantity' => 3,
+            ],
+            [
+                'product_id' => 24,
+                'size_id' => 4,
+                'quantity' => 2,
+            ],
+            [
+                'product_id' => 25,
+                'size_id' => 1,
+                'quantity' => 4,
+            ],
+            [
+                'product_id' => 25,
+                'size_id' => 2,
+                'quantity' => 2,
+            ],
+            [
+                'product_id' => 25,
+                'size_id' => 3,
+                'quantity' => 3,
+            ],
+            [
+                'product_id' => 25,
+                'size_id' => 4,
+                'quantity' => 4,
+            ],
+            [
+                'product_id' => 26,
+                'size_id' => 5,
+                'quantity' => 2,
+            ],
+            [
+                'product_id' => 27,
+                'size_id' => 1,
+                'quantity' => 6,
+            ],
+            [
+                'product_id' => 27,
+                'size_id' => 2,
+                'quantity' => 1,
+            ],
+            [
+                'product_id' => 27,
+                'size_id' => 3,
+                'quantity' => 5,
+            ],
+            [
+                'product_id' => 28,
+                'size_id' => 1,
+                'quantity' => 3,
+            ],
+            [
+                'product_id' => 28,
+                'size_id' => 2,
+                'quantity' => 2,
+            ],
+            [
+                'product_id' => 28,
+                'size_id' => 3,
+                'quantity' => 4,
+            ],
+            [
+                'product_id' => 29,
+                'size_id' => 1,
+                'quantity' => 2,
+            ],
+            [
+                'product_id' => 29,
+                'size_id' => 2,
+                'quantity' => 3,
+            ],
+            [
+                'product_id' => 29,
+                'size_id' => 3,
+                'quantity' => 4,
+            ],
+            [
+                'product_id' => 30,
+                'size_id' => 1,
+                'quantity' => 2,
+            ],
+            [
+                'product_id' => 30,
+                'size_id' => 2,
+                'quantity' => 6,
+            ],
+            [
+                'product_id' => 30,
+                'size_id' => 3,
+                'quantity' => 1,
+            ],
+            [
+                'product_id' => 31,
+                'size_id' => 1,
+                'quantity' => 5,
+            ],
+            [
+                'product_id' => 31,
+                'size_id' => 2,
+                'quantity' => 3,
+            ],
+            [
+                'product_id' => 31,
+                'size_id' => 3,
+                'quantity' => 2,
+            ],
+            [
+                'product_id' => 32,
+                'size_id' => 1,
+                'quantity' => 4,
+            ],
+            [
+                'product_id' => 32,
+                'size_id' => 2,
+                'quantity' => 2,
+            ],
+            [
+                'product_id' => 32,
+                'size_id' => 3,
+                'quantity' => 3,
+            ],
+            [
+                'product_id' => 33,
+                'size_id' => 1,
+                'quantity' => 4,
+            ],
+            [
+                'product_id' => 33,
+                'size_id' => 2,
+                'quantity' => 2,
+            ],
+            [
+                'product_id' => 33,
+                'size_id' => 3,
+                'quantity' => 6,
+            ],
+            [
+                'product_id' => 33,
+                'size_id' => 4,
+                'quantity' => 1,
+            ],
+            [
+                'product_id' => 34,
+                'size_id' => 1,
+                'quantity' => 5,
+            ],
+            [
+                'product_id' => 34,
+                'size_id' => 2,
+                'quantity' => 3,
+            ],
+            [
+                'product_id' => 34,
+                'size_id' => 3,
+                'quantity' => 2,
+            ],
+            [
+                'product_id' => 34,
+                'size_id' => 4,
+                'quantity' => 4,
+            ],
+            [
+                'product_id' => 35,
+                'size_id' => 1,
+                'quantity' => 2,
+            ],
+            [
+                'product_id' => 35,
+                'size_id' => 2,
+                'quantity' => 3,
+            ],
+            [
+                'product_id' => 35,
+                'size_id' => 3,
+                'quantity' => 4,
+            ],
+            [
+                'product_id' => 35,
+                'size_id' => 4,
+                'quantity' => 2,
+            ],
+            [
+                'product_id' => 36,
+                'size_id' => 1,
+                'quantity' => 6,
+            ],
+            [
+                'product_id' => 36,
+                'size_id' => 2,
+                'quantity' => 1,
+            ],
+            [
+                'product_id' => 36,
+                'size_id' => 3,
+                'quantity' => 5,
+            ],
+            [
+                'product_id' => 36,
+                'size_id' => 4,
+                'quantity' => 3,
+            ],
+            [
+                'product_id' => 37,
+                'size_id' => 1,
+                'quantity' => 2,
+            ],
+            [
+                'product_id' => 37,
+                'size_id' => 2,
+                'quantity' => 4,
+            ],
+            [
+                'product_id' => 37,
+                'size_id' => 3,
+                'quantity' => 2,
+            ],
+            [
+                'product_id' => 37,
+                'size_id' => 4,
+                'quantity' => 3,
+            ],
+            [
+                'product_id' => 38,
+                'size_id' => 1,
+                'quantity' => 4,
+            ],
+            [
+                'product_id' => 38,
+                'size_id' => 2,
+                'quantity' => 2,
+            ],
+            [
+                'product_id' => 38,
+                'size_id' => 3,
+                'quantity' => 6,
+            ],
+            [
+                'product_id' => 38,
+                'size_id' => 4,
+                'quantity' => 1,
+            ],
+            [
+                'product_id' => 39,
+                'size_id' => 1,
+                'quantity' => 5,
+            ],
+            [
+                'product_id' => 39,
+                'size_id' => 2,
+                'quantity' => 2,
+            ],
+            [
+                'product_id' => 39,
+                'size_id' => 3,
+                'quantity' => 3,
+            ],
+            [
+                'product_id' => 39,
+                'size_id' => 4,
+                'quantity' => 4,
+            ],
+            [
+                'product_id' => 40,
+                'size_id' => 1,
+                'quantity' => 2,
+            ],
+            [
+                'product_id' => 40,
+                'size_id' => 2,
+                'quantity' => 3,
+            ],
+            [
+                'product_id' => 40,
+                'size_id' => 3,
+                'quantity' => 4,
+            ],
+            [
+                'product_id' => 40,
+                'size_id' => 4,
+                'quantity' => 2,
+            ],
         ]);
         DB::statement('SET FOREIGN_KEY_CHECKS = 1');
     }
