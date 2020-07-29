fe1bd366b78b0d00d3fb4ea9ea1252c05e0d39f8
Nguyễn Đăng Bằng <nguyendangbang@MacBook-Pro-cua-Nguyen.local>
2020-07-29 21:03:15 +0700

    up web.php

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $categories = \App\Category::all()->where('status','=',1);
+    $collections = \App\Collection::all()->where('status','=',1);
+    $products = \App\Product::all()->where('status','=',1);
+    return view('frontend.index',['categories'=>$categories,'collections'=>$collections,'products'=>$products]);
 });
+
