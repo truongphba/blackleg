59d20b29f654ccf4a2896b83e418bef67427e5c9
quannguyen1778 <59248721+quannguyen1778@users.noreply.github.com>
2020-07-31 19:09:19 +0700

    sua lai seeder

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -17,48 +17,40 @@ class CategorySeeder extends Seeder
         DB::table('categories')->insert([
             [
                 'id' => 1,
-                'name' => 'Áo Sơ Mi',
-                'thumbnail' => 'https://ferosh.vn/uploads/14-08-2019/420x630/V61B19H049-RSS51--M64T19Q010-P4115--2-.jpg',
+                'name' => 'Women Shirt',
+                'thumbnail' => 'https://scontent.fsgn2-3.fna.fbcdn.net/v/t1.0-9/109695604_975238502946532_8464704805258571631_o.jpg?_nc_cat=108&_nc_sid=e3f864&_nc_ohc=yjNI5U-jg2YAX8t_S1l&_nc_ht=scontent.fsgn2-3.fna&oh=bff7cd019d3aa7d89050ed104b6a63f0&oe=5F4B36C2',
+                'status' => 1,
+                'created_at' => \Illuminate\Support\Carbon::now()->addDays(-3)->format('Y-m-d H:i:s'),
+                'updated_at' => \Illuminate\Support\Carbon::now()->addDays(-2)->format('Y-m-d H:i:s')
+            ],
+            [
+                'id' => 2,
+                'name' => 'Dress',
+                'thumbnail' => 'https://scontent.fsgn2-6.fna.fbcdn.net/v/t1.0-9/97998125_882219942259327_3914516795112816640_o.jpg?_nc_cat=110&_nc_sid=e3f864&_nc_ohc=ecpU7D9tV2oAX-QHqyW&_nc_ht=scontent.fsgn2-6.fna&oh=4fa5ef740632f9f10a47423cd111fc41&oe=5F4B04C2',
                 'status' => 1,
                 'created_at' => \Illuminate\Support\Carbon::now()->addDays(-3)->format('Y-m-d H:i:s'),
                 'updated_at' => \Illuminate\Support\Carbon::now()->addDays(-2)->format('Y-m-d H:i:s')
             ],
             [
                 'id' => 3,
-                'name' => 'Đầm',
-                'thumbnail' => 'https://ferosh.vn/uploads/07-09-2019/V63R19Q001-R1P24--2-(1).jpg',
+                'name' => 'Jumpsuit',
+                'thumbnail' => 'https://scontent.fsgn2-1.fna.fbcdn.net/v/t1.0-9/100747056_1097171943972021_4634036126292639744_o.jpg?_nc_cat=107&_nc_sid=e3f864&_nc_ohc=YoEwLJiQhbMAX8CfRX4&_nc_oc=AQnhQNiuLN9SIDbA8qgh5SFA38Zvbpzvoh-CRuPD3a_x6VpSpMyP6y4UknOtPfB0ioI&_nc_ht=scontent.fsgn2-1.fna&oh=e9e40131be5eeecbebba5de5e874580d&oe=5F4AF86F',
                 'status' => 1,
                 'created_at' => \Illuminate\Support\Carbon::now()->addDays(-3)->format('Y-m-d H:i:s'),
                 'updated_at' => \Illuminate\Support\Carbon::now()->addDays(-2)->format('Y-m-d H:i:s')
             ],
             [
                 'id' => 4,
-                'name' => 'Váy',
-                'thumbnail' => 'https://ferosh.vn/uploads/05-09-2019/L63F19Q026-F1814--1-.jpg',
+                'name' => 'Skirt',
+                'thumbnail' => 'https://s3-ap-southeast-1.amazonaws.com/canifablog/blog/wp-content/uploads/2019/10/05082548/ch%C3%A2n-v%C3%A1y-d%C3%A0i-m%E1%BA%B7c-m%C3%B9a-%C4%91%C3%B4ng-2019.jpg',
                 'status' => 1,
                 'created_at' => \Illuminate\Support\Carbon::now()->addDays(-3)->format('Y-m-d H:i:s'),
                 'updated_at' => \Illuminate\Support\Carbon::now()->addDays(-2)->format('Y-m-d H:i:s')
             ],
             [
                 'id' => 5,
-                'name' => 'Jumpsuit',
-                'thumbnail' => 'https://ferosh.vn/storage/images/7532e11ff40e244cedde99f723f5e882/525x787/duV5oye5kGOP51g6ixnrgoHCDfMPQKDb5gaPPikY.jpeg',
-                'status' => 1,
-                'created_at' => \Illuminate\Support\Carbon::now()->addDays(-3)->format('Y-m-d H:i:s'),
-                'updated_at' => \Illuminate\Support\Carbon::now()->addDays(-2)->format('Y-m-d H:i:s')
-            ],
-            [
-                'id' => 6,
-                'name' => 'Chân váy',
-                'thumbnail' => 'https://ferosh.vn/uploads/02-11-2017/De%20Leah-Chan-vay-mini-can-beo-gau-xanh-tim-007.jpg',
-                'status' => 1,
-                'created_at' => \Illuminate\Support\Carbon::now()->addDays(-3)->format('Y-m-d H:i:s'),
-                'updated_at' => \Illuminate\Support\Carbon::now()->addDays(-2)->format('Y-m-d H:i:s')
-            ],
-            [
-                'id' => 7,
-                'name' => 'Đồ ngủ',
-                'thumbnail' => 'https://ferosh.vn/storage/images/c090dfe166fa2d6830fe7eec1bd06b73/525x787/TaFPosCQ0VJlCwgI4AsDTASRoXtrHYzn6NZLNyyt.jpeg',
+                'name' => 'Sleepwear',
+                'thumbnail' => 'https://scontent.fsgn2-1.fna.fbcdn.net/v/t1.0-9/116073538_3228720017173597_769423600697365999_o.jpg?_nc_cat=107&_nc_sid=e3f864&_nc_ohc=WokY1wkbUS8AX_twfSw&_nc_ht=scontent.fsgn2-1.fna&oh=719cef66fc12ce682b156fab961cb178&oe=5F4976DF',
                 'status' => 1,
                 'created_at' => \Illuminate\Support\Carbon::now()->addDays(-3)->format('Y-m-d H:i:s'),
                 'updated_at' => \Illuminate\Support\Carbon::now()->addDays(-2)->format('Y-m-d H:i:s')
--- database/seeds/ProductSeeder.php
+++ database/seeds/ProductSeeder.php
@@ -23,8 +23,7 @@ class ProductSeeder extends Seeder
                 'thumbnail' => 'https://ferosh.vn/storage/images/bc140cb5cb4c500db60537d94455756b/420x630/pB65Wcry3zGeeFe91CoM3LhQwwKEtfWtMBkK0oaT.jpeg',
                 'colors' => 'Rêu',
                 'description' => '',
-                'detail' => '- Sản phẩm: Không lót</br>
-                             - Chất liệu: 40% Viscose 60% polyester.</br>
+                'detail' => '- Chất liệu: 40% Viscose 60% polyester.</br>
                              - Lót: Không</br>
                              - Co giãn: Không',
                 'highlight' => 0,
@@ -34,13 +33,13 @@ class ProductSeeder extends Seeder
             ],
             [
                 'id' => 2,
-                'category_id' => 3,
+                'category_id' => 2,
                 'name' => 'ĐẦM KẺ XUÔNG BÈO GẤU KẺ NÂU',
                 'price' => '885000',
                 'thumbnail' => 'https://ferosh.vn/storage/images/0c28423a88b5dbffd0d1ee2aec41efd0/420x630/RrPhuNQc1LiOz64MVF9cZVOlFGMHBaUXMJThvI1l.jpeg',
                 'colorss' => 'Kẻ nâu',
                 'description' => '',
-                'detail' => '- Sản phẩm: Có lót.</br>
+                'detail' => '- Lót: Có</br>
                              - Chất liệu: Thô Linen Hàn Quốc.</br>
                              - Co giãn: Không',
                 'highlight' => 1,
@@ -50,7 +49,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'id' => 3,
-                'category_id' => 5,
+                'category_id' => 3,
                 'name' => 'JUMPSUIT SOOC VẠT CHỒNG HOA NỀN XANH BIỂN',
                 'price' => '745000',
                 'thumbnail' => 'https://ferosh.vn/storage/images/9d2596a5514f8021eaf84fc7e42f792b/420x630/pR6DDDFSpl2QCILHzVzwDiq6N0ktvPIQ06I6Oern.jpeg',
@@ -67,7 +66,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'id' => 4,
-                'category_id' => 5,
+                'category_id' => 3,
                 'name' => 'JUMPSUIT SOOC VẠT CHỒNG HOA NỀN TÍM THAN',
                 'price' => '745000',
                 'thumbnail' => 'https://ferosh.vn/storage/images/66a7bb0bfc7f4f952d518528c1c7e024/420x630/zwdag2m5k13NirHPQRS3ihKRY07VvsOPZ3yKhPYz.jpeg',
@@ -84,7 +83,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'id' => 5,
-                'category_id' => 3,
+                'category_id' => 2,
                 'name' => 'ĐẦM LIỀN BUỘC KHĂN CỔ CHÂN A XANH RÊU',
                 'price' => '1397000 ',
                 'thumbnail' => 'https://ferosh.vn/storage/images/912527e69aef16ac97324684dda53015/420x630/DbPEVnJpRDKw09OC6PAAGa4q6buFoZjR5wTNQdrq.jpeg',
@@ -100,7 +99,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'id' => 6,
-                'category_id' => 3,
+                'category_id' => 2,
                 'name' => 'ĐẦM LIỀN BUỘC KHĂN CỔ CHÂN A ĐỎ ĐÔ',
                 'price' => '1397000 ',
                 'thumbnail' => 'https://ferosh.vn/storage/images/0f796eb02238d62a7ace5b8f2c416d91/420x630/35PGrsCIbpWDEhmfn03LF5Pa4gjrFOD349J774Tp.jpeg',
@@ -116,7 +115,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'id' => 7,
-                'category_id' => 3,
+                'category_id' => 2,
                 'name' => 'ĐẦM CỔ TIM, XẾP CHỤM THÂN TRƯỚC XANH BIỂN',
                 'price' => '990000 ',
                 'thumbnail' => 'https://ferosh.vn/uploads/13-09-2019/420x630/DRE1062-5.jpg',
@@ -148,7 +147,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'id' => 9,
-                'category_id' => 3,
+                'category_id' => 2,
                 'name' => 'ĐẦM REN CHOÀNG TRẮNG VÂN ĐEN',
                 'price' => '1750000 ',
                 'thumbnail' => 'https://ferosh.vn/uploads/16-01-2020/420x630/IMGL1981.jpg',
@@ -165,7 +164,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'id' => 10,
-                'category_id' => 3,
+                'category_id' => 2,
                 'name' => 'ĐẦM BODY TAY BỒNG HOA EO',
                 'price' => '1980000 ',
                 'thumbnail' => 'https://ferosh.vn/uploads/16-01-2020/420x630/IMGL2126.jpg',
@@ -182,7 +181,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'id' => 11,
-                'category_id' => 3,
+                'category_id' => 2,
                 'name' => 'ĐẦM BODY HOA XOẮN VAI',
                 'price' => '1750000 ',
                 'thumbnail' => 'https://ferosh.vn/uploads/16-01-2020/420x630/IMGL1761.jpg',
@@ -199,7 +198,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'id' => 12,
-                'category_id' => 3,
+                'category_id' => 2,
                 'name' => 'ĐẦM REN CHOÀNG ĐÍNH NGỌC TRAI ĐEN',
                 'price' => '1860000 ',
                 'thumbnail' => 'https://ferosh.vn/uploads/16-01-2020/420x630/IMGL1368.jpg',
@@ -216,7 +215,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'id' => 13,
-                'category_id' => 3,
+                'category_id' => 2,
                 'name' => 'ĐẦM RỚT VAI CÀI HOA CẨM TÚ',
                 'price' => '1890000 ',
                 'thumbnail' => 'https://ferosh.vn/uploads/16-01-2020/420x630/IMGL2014.jpg',
@@ -233,7 +232,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'id' => 14,
-                'category_id' => 3,
+                'category_id' => 2,
                 'name' => 'ĐẦM XẾP NẾP VÒNG CỔ LASER',
                 'price' => '1750000 ',
                 'thumbnail' => 'https://ferosh.vn/uploads/16-01-2020/420x630/IMGL2204.jpg',
@@ -250,7 +249,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'id' => 15,
-                'category_id' => 3,
+                'category_id' => 2,
                 'name' => 'ĐẦM SÁT NÁCH CẮT LASER ĐEN',
                 'price' => '1560000 ',
                 'thumbnail' => 'https://ferosh.vn/uploads/23-12-2019/420x630/-34A8389.jpg',
@@ -267,7 +266,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'id' => 16,
-                'category_id' => 6,
+                'category_id' => 4,
                 'name' => 'CHÂN VÁY MIDI LASER HỒNG ĐẬM',
                 'price' => '790000 ',
                 'thumbnail' => 'https://ferosh.vn/uploads/23-12-2019/420x630/-34A8308(1).jpg',
@@ -284,7 +283,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'id' => 17,
-                'category_id' => 6,
+                'category_id' => 4,
                 'name' => 'CHÂN VÁY MIDI VẠT LỆCH XANH CỔ VỊT',
                 'price' => '790000 ',
                 'thumbnail' => 'https://ferosh.vn/uploads/23-12-2019/420x630/-34A8303(1).jpg',
@@ -301,7 +300,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'id' => 18,
-                'category_id' => 6,
+                'category_id' => 4,
                 'name' => 'COFASON CHÂN VÁY A CARO XANH MINT',
                 'price' => '790000 ',
                 'thumbnail' => 'https://ferosh.vn/uploads/09-11-2019/420x630/3M3A8351.jpg',
@@ -318,7 +317,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'id' => 19,
-                'category_id' => 3,
+                'category_id' => 2,
                 'name' => 'ĐẦM XÒE LỤA NỀN XANH HAI DÂY',
                 'price' => '1499000',
                 'thumbnail' => 'https://ferosh.vn/storage/images/d94500cbf74c4ea234acc780c04913a2/525x787/P7clnBppXmthRsLt8OeB4OUAWtGOFzFFRWxgTAlM.jpeg',
@@ -334,7 +333,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'id' => 20,
-                'category_id' => 3,
+                'category_id' => 2,
                 'name' => 'ĐẦM LỤA XÒE TRẮNG',
                 'price' => '1099000',
                 'thumbnail' => 'https://ferosh.vn/storage/images/2188a2fe8f909b85070d055dfe363142/525x787/BZZxgrbycyr6gxWepaMS9aKzeoFiZJ06Qz7CLWKm.jpeg',
@@ -350,7 +349,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'id' => 21,
-                'category_id' => 3,
+                'category_id' => 2,
                 'name' => 'ĐẦM XÒE THÔ CÓ DÂY ĐAI',
                 'price' => '1399000',
                 'thumbnail' => 'https://ferosh.vn/storage/images/8646de4bf4a5dc60490bf6479ada9250/525x787/C8GzJ6BJh22jnzfTxYvSJ400fJIHWAkT1oLUX6zw.jpeg',
@@ -366,7 +365,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'id' => 22,
-                'category_id' => 3,
+                'category_id' => 2,
                 'name' => 'ĐẦM THÔ KHOÉT NGỰC',
                 'price' => '1399000',
                 'thumbnail' => 'https://ferosh.vn/storage/images/2ef1bbdc45ad8e0abaa18d187033bca2/525x787/CjneRHpR0Rf7HDp95fDV05xB57BbT7krw2dHs5Ub.jpeg',
@@ -382,7 +381,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'id' => 23,
-                'category_id' => 3,
+                'category_id' => 2,
                 'name' => 'ĐẦM THÔ DÁNG A SÁT NÁCH TUA RUA GẤU',
                 'price' => '1499000',
                 'thumbnail' => 'https://ferosh.vn/storage/images/f133b30511fd4fcb683d685de5514bc7/525x787/70uGXzF1ITd9PRnNrywjzCSxknqd9e0ALYf2CHVC.jpeg',
@@ -398,7 +397,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'id' => 24,
-                'category_id' => 3,
+                'category_id' => 2,
                 'name' => 'ĐẦM THÔ SUÔNG XANH NAVY HỌA TIẾT',
                 'price' => '1499000',
                 'thumbnail' => 'https://ferosh.vn/storage/images/8fc2ff25a36a84a76e695d7a30819c21/525x787/igsPT7sR7pwGKlXIqHXWX9ypFa7qYOztHpEmCIRT.jpeg',
@@ -414,7 +413,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'id' => 25,
-                'category_id' => 3,
+                'category_id' => 2,
                 'name' => 'ĐẦM LỤA ÔM DÂY LƯNG',
                 'price' => '1299000',
                 'thumbnail' => 'https://ferosh.vn/storage/images/96f10132748c7049e3bbf5ca0b275f18/525x787/YmKsB5dNUwUt77E1ydq2NEIkJLOxKYP9A0QvGC2g.jpeg',
@@ -446,7 +445,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'id' => 27,
-                'category_id' => 4,
+                'category_id' => 2,
                 'name' => 'VÁY HỒNG XOẮN EO HỒNG',
                 'price' => '790000',
                 'thumbnail' => 'https://ferosh.vn/storage/images/80b34c84a6e5ed25df112c11de676adc/420x630/DsZ6QXsaWdMBJnMs3zsO7cypoJwwTNMMLFLmYsag.jpeg',
@@ -479,7 +478,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'id' => 29,
-                'category_id' => 4,
+                'category_id' => 2,
                 'name' => 'VÁY COTTON TRỄ VAI',
                 'price' => '720000',
                 'thumbnail' => 'https://ferosh.vn/storage/images/a463cac2327534f6f02563ffbdf92918/420x630/vPFV6bPPT9DRGYkIc4x4IKotZpub2hgZyhYwf18U.jpeg',
@@ -495,7 +494,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'id' => 30,
-                'category_id' => 5,
+                'category_id' => 3,
                 'name' => 'JUMPSUIT ĐŨI CHUN EO HỒNG',
                 'price' => '830000',
                 'thumbnail' => 'https://ferosh.vn/storage/images/7532e11ff40e244cedde99f723f5e882/420x630/duV5oye5kGOP51g6ixnrgoHCDfMPQKDb5gaPPikY.jpeg',
@@ -511,7 +510,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'id' => 31,
-                'category_id' => 5,
+                'category_id' => 3,
                 'name' => 'JUMPSUIT ĐŨI CHUN EO ĐỎ',
                 'price' => '830000',
                 'thumbnail' => 'https://ferosh.vn/storage/images/f7b80214c9564ac1bd11fd91a037d17b/420x630/dkPnBxuksQncpDrmuwug8s1zHw8XB9Dbv4KNUUGE.jpeg',
@@ -527,7 +526,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'id' => 32,
-                'category_id' => 4,
+                'category_id' => 2,
                 'name' => 'VÁY BÈO VAI HOA XANH',
                 'price' => '680000',
                 'thumbnail' => 'https://ferosh.vn/storage/images/7f65d29fa86be6c6036fd73bae7e3cd3/420x630/ne42CgceEMHkypxYDPYx9b5thIFZ5iK463CMMsME.jpeg',
@@ -543,7 +542,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'id' => 33,
-                'category_id' => 7,
+                'category_id' => 5,
                 'name' => 'BỘ MẶC NHÀ LỤA QUẦN LỬNG ÁO CỘC TAY TÍM THAN',
                 'price' => '459000',
                 'thumbnail' => 'https://ferosh.vn/storage/images/e575af3f6d64da1287b8b6552b65a1df/420x630/EihzfYUVG2mVYyaLt0SgLn23N3El0vjwjsc441sa.jpeg',
@@ -559,7 +558,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'id' => 34,
-                'category_id' => 7,
+                'category_id' => 5,
                 'name' => 'BỘ MẶC NHÀ LỤA QUẦN LỬNG ÁO CỘC TAY TÍM THAN',
                 'price' => '489000',
                 'thumbnail' => 'https://ferosh.vn/storage/images/9dc0a5e5bfcf4d73878002040de152ae/420x630/lqqIp9kceUhAAL4XvM8AFWp0dODFEtnJSsvv3uw4.jpeg',
@@ -575,7 +574,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'id' => 35,
-                'category_id' => 7,
+                'category_id' => 5,
                 'name' => 'BỘ MẶC NHÀ LỤA HAI DÂY QUẦN LỬNG',
                 'price' => '429000',
                 'thumbnail' => 'https://ferosh.vn/storage/images/67efce7bbed5fa3ea002e27e0387fa1e/420x630/aNigUJwuYtmfBnLVpzRz3gHRMYDp6r2YvTgDGmYb.jpeg',
@@ -591,7 +590,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'id' => 36,
-                'category_id' => 7,
+                'category_id' => 5,
                 'name' => 'BỘ MẶC NHÀ COTTON ÁO CỘC TAY QUẦN LỬNG RÊU',
                 'price' => '379000',
                 'thumbnail' => 'https://ferosh.vn/storage/images/247ad943f6dde95b3429e21616e96ae9/420x630/yTo5bM4xzB6tvbZinGfMZr5S0eRRhDn1OmtHuYKp.jpeg',
@@ -607,7 +606,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'id' => 37,
-                'category_id' => 7,
+                'category_id' => 5,
                 'name' => 'BỘ MẶC NHÀ LỤA QUẦN ĐÙI ÁO HAI DÂY XANH THAN',
                 'price' => '399000',
                 'thumbnail' => 'https://ferosh.vn/storage/images/8c3429b990429dca8fbe1cd6cf2e9636/420x630/hSmF3P6bl9QOFAGDWTRpN4SwVtwO5kh27YPRXkRE.jpeg',
@@ -623,7 +622,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'id' => 38,
-                'category_id' => 7,
+                'category_id' => 5,
                 'name' => 'BỘ MẶC NHÀ LỤA QUẦN ĐÙI ÁO HAI DÂY HỒNG ĐẬM',
                 'price' => '399000',
                 'thumbnail' => 'https://ferosh.vn/storage/images/3398f11e14b079df6c5b2e2541ea641e/420x630/JgRm49f67In5tme2DOUgwTfGvNdOCEiZQT3nY9l5.jpeg',
@@ -639,7 +638,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'id' => 39,
-                'category_id' => 7,
+                'category_id' => 5,
                 'name' => 'BỘ MẶC NHÀ LỤA QUẦN ĐÙI ÁO CỘC TAY',
                 'price' => '389000',
                 'thumbnail' => 'https://ferosh.vn/storage/images/07a498d2be79102ce2ca947bae2c53eb/420x630/fJyPqUAyHMDxw5oZzPCGwdUdPxiKeANrv9BTvyCi.jpeg',
@@ -655,7 +654,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'id' => 40,
-                'category_id' => 7,
+                'category_id' => 5,
                 'name' => 'BỘ MẶC NHÀ LỤA QUẦN ĐÙI ÁO CỘC TAY XANH THAN',
                 'price' => '389000',
                 'thumbnail' => 'https://ferosh.vn/storage/images/58111ca2842fcb35ecbfd51fd89eb79a/420x630/F06Xc0F3Q2qdKkfqphdupHUCaoimwoUAdM1eGJgb.jpeg',
