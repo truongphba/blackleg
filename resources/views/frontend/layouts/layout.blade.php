1a04f7a348be39076af0e1a0073456b6555a3692
Nguyen Dang Bang <bang@gmail.com>
2020-08-07 19:25:46 +0700

    up

--- resources/views/frontend/layouts/layout.blade.php
+++ resources/views/frontend/layouts/layout.blade.php
@@ -75,12 +75,14 @@
                     <ul class="nav navbar-nav">
                         @foreach([
                             (object)['name'=>'Home','link'=>'/','sub'=>''],
+                            (object)['name'=>'About','link'=>'/','sub'=>''],
                             (object)['name'=>'Sale','link'=>'#','sub'=>''],
                             (object)['name'=>'Category','link'=>'#','sub'=>$categories],
                             (object)['name'=>'Collection','link'=>'#','sub'=>$collections],
                             (object)['name'=>'Q&A','link'=>'#','sub'=>''],
                             (object)['name'=>'Contact','link'=>'#','sub'=>''],
                         ] as $i=>$obj)
+
                             <li><a href="{{$obj->link}}">{{$obj->name}}<span class="{{$obj->sub?'caret':'dn'}}"></span></a>
                                 @if($obj->sub)
                                     <ul class="dropdown-menu">
@@ -108,29 +110,29 @@
 <!-- / footer -->
 
 <!-- Login Modal -->
-<div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-body">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4>Login or Register</h4>
-                <form class="aa-login-form" action="">
-                    <label for="">Username or Email address<span>*</span></label>
-                    <input type="text" placeholder="Username or email">
-                    <label for="">Password<span>*</span></label>
-                    <input type="password" placeholder="Password">
-                    <button class="aa-browse-btn" type="submit">Login</button>
-                    <label for="rememberme" class="rememberme"><input type="checkbox" id="rememberme"> Remember me
-                    </label>
-                    <p class="aa-lost-password"><a href="#">Lost your password?</a></p>
-                    <div class="aa-register-now">
-                        Don't have an account?<a href="account.html">Register now!</a>
-                    </div>
-                </form>
-            </div>
-        </div><!-- /.modal-content -->
-    </div><!-- /.modal-dialog -->
-</div>
+{{--<div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">--}}
+{{--    <div class="modal-dialog">--}}
+{{--        <div class="modal-content">--}}
+{{--            <div class="modal-body">--}}
+{{--                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>--}}
+{{--                <h4>Login or Register</h4>--}}
+{{--                <form class="aa-login-form" action="">--}}
+{{--                    <label for="">Username or Email address<span>*</span></label>--}}
+{{--                    <input type="text" placeholder="Username or email">--}}
+{{--                    <label for="">Password<span>*</span></label>--}}
+{{--                    <input type="password" placeholder="Password">--}}
+{{--                    <button class="aa-browse-btn" type="submit">Login</button>--}}
+{{--                    <label for="rememberme" class="rememberme"><input type="checkbox" id="rememberme"> Remember me--}}
+{{--                    </label>--}}
+{{--                    <p class="aa-lost-password"><a href="#">Lost your password?</a></p>--}}
+{{--                    <div class="aa-register-now">--}}
+{{--                        Don't have an account?<a href="account.html">Register now!</a>--}}
+{{--                    </div>--}}
+{{--                </form>--}}
+{{--            </div>--}}
+{{--        </div><!-- /.modal-content -->--}}
+{{--    </div><!-- /.modal-dialog -->--}}
+{{--</div>--}}
 
 
 <!--[if lt IE 9]>
